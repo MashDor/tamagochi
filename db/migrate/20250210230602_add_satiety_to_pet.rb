833e19ee8cc75df39b57f85f503d407961fd1f15
Maria <m.dorofeeva@slurm.io>
2025-02-12 02:48:10 +0500

    congif sidekiq and redis, create first job

--- db/migrate/20250210230602_add_satiety_to_pet.rb
+++ db/migrate/20250210230602_add_satiety_to_pet.rb
@@ -1,5 +1,5 @@
 class AddSatietyToPet < ActiveRecord::Migration[8.0]
   def change
-    add_column :pets, :satiety, :integer, default: 50
+    add_column :pets, :satiety, :integer, default: 50, max: 100, min: 0
   end
 end
---
+++ test/jobs/satiety_reminder_job_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SatietyReminderJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
