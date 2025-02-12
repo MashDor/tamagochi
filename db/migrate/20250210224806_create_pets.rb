e953374ab6f88600ac5198e8e8aaeabbcaab9ba4
Maria <m.dorofeeva@slurm.io>
2025-02-12 05:05:47 +0500

    squash migrations

--- db/migrate/20250210224806_create_pets.rb
+++ db/migrate/20250210224806_create_pets.rb
@@ -1,6 +1,10 @@
 class CreatePets < ActiveRecord::Migration[8.0]
   def change
     create_table :pets do |t|
+      t.integer :satiety, :integer, default: 50, max: 100, min: 0
+      t.datetime :last_fed_at, :datetime, default: -> { 'CURRENT_TIMESTAMP' }
+      t.references :user, null: false,  foreign_key: true
+
       t.timestamps
     end
   end
--- db/migrate/20250210230602_add_satiety_to_pet.rb
+++
@@ -1,5 +0,0 @@
-class AddSatietyToPet < ActiveRecord::Migration[8.0]
-  def change
-    add_column :pets, :satiety, :integer, default: 50, max: 100, min: 0
-  end
-end
